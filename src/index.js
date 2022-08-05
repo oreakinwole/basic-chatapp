2914ca74ec4cb87b722dd3a5362c159d2bd60570
Ore Akinwole <oreakinwole@gmail.com>
2022-08-05 22:23:24 +0100

    chore: init redux reducer

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { legacy_createStore as CreateStore } from "redux";
+import mainReducer from "./reducer";
+import App from "./app";
+
+const store = CreateStore(mainReducer);
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
